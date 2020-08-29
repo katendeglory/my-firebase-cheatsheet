9cbc9ac1d9c249ab95d0d54846b89a31dc33e3fe
Glory Katende <katendeglory@gmail.com>
2020-08-29 10:09:41 +0200

    Async/Await

--- backend.js
+++ backend.js
@@ -4,22 +4,16 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.addAdminRole = functions.https.onCall((data, context) => {
-  // Check if request is made by an admin
-  if (!context.auth.token.admin) {
-    return { error: "Only admins can add other admins" }
-  }
+exports.addAdminRole = functions.https.onCall(async (data, context) => {
+  try {
+    if (!context.auth.token.admin) return { error: "Only admins can add other admins" }
 
-  // Get user and add admin custom claim
-  return admin.auth().getUserByEmail(data.email).then(user => {
-    return admin.auth().setCustomUserClaims(user.uid, {
-      admin: true
-    })
-  }).then(() => {
-    return {
-      message: `Success! ${data.email} has been made an admin.`
-    }
-  }).catch(err => {
-    return err;
-  });
+    let user = await admin.auth().getUserByEmail(data.email);
+    let res = await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+
+    return { message: `Success! ${data.email} has been made an admin.` };
+
+  } catch (error) {
+    return { error };
+  }
 });
